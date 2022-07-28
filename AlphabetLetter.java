47ea9eb2d70ffa95d77fe77fc5d00a5ef797fb27
Mendel Ba <33570785+MENDEL-BA@users.noreply.github.com>
2022-07-28 13:23:40 +0000

    Initial commit

---
+++ AlphabetLetter.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+      
+    for(int i = 0 ; i <=26; i++){
+       
+     System.out.println(" " + (char)(i + 'a' - 1));
+      System.out.println("\t ");
+     }
+  }
+}
